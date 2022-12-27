c39e1dc17e15af0f11b93e5de67044c3d5e52c32
sayedmdabu <sayedmdabu94@gmail.com>
2022-12-27 22:01:55 +0600

    27-12-2022

--- django_1/first/first/settings.py
+++ django_1/first/first/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'first.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR, ""],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
